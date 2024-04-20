6c735faa519d5f9de8243020e89f256d4afc39d4
AntheaJFW <wong.ji.fong@gmail.com>
2024-04-21 01:39:24 +0800

    revert outline

--- generate_gh_page.sh
+++ generate_gh_page.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-jupyter nbconvert --output-dir='./docs' --output='output' --to html IT5100F_project_assnmt.ipynb
+jupyter nbconvert --output-dir='./docs' --output='index' --to html IT5100F_project_assnmt.ipynb
